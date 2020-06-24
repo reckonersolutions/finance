1135ef1f642d2a6a2ed6831f1a9a0d8321ba7114
reckonersolutions <solutions.reckoner@gmail.com>
2020-06-24 17:43:02 +0400

    1742 24062020

---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+<<<<<<< HEAD
+      dir: require('path').join(__dirname, './coverage/finance'),
+=======
+<<<<<<< HEAD
+      dir: require('path').join(__dirname, './coverage/finance'),
+=======
+      dir: require('path').join(__dirname, './coverage/dashboard050620'),
+>>>>>>> 8a9d80c44bb2d048c60587e169a08a1dd079a5df
+>>>>>>> a66e71ac5d35bdcb9563aa230928325c1f232f93
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
